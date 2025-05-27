45797e68b2669f0c038e8d9398d0e5c5ade480c3
namlvhe172313 <namlvhe172313@fpt.edu.vn>
2025-05-27 10:56:42 +0700

    uyt

--- FRONTEND/src/component/HomePage/HomePage.jsx
+++ FRONTEND/src/component/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1 className='text-center'>HELLO mn!!</h1>
+      <h1 className='text-center'>HELLO mn!!!</h1>
     </div>
   )
 }
