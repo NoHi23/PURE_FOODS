fc55b483cd32a5f878033b85697ceab67c494139
anhNQ279 <anhnqhe181014@fpt.edu.vn>
2025-06-14 23:29:40 +0700

    Create DeliveryService.java

---
+++ BACKEND/src/main/java/com/spring/service/DeliveryService.java
@@ -0,0 +1,34 @@
+package com.spring.service;
+
+import com.spring.entity.Drivers;
+import com.spring.entity.Orders;
+import com.spring.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.spring.entity.OrderStatuses;
+import java.sql.Timestamp;
+import java.util.Optional;
+
+@Service
+public class DeliveryService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+   public Orders prepareDelivery(Integer orderId, Timestamp estimatedDeliveryDate) {
+    Orders order = orderRepository.findByOrderId(orderId);
+    if (order != null) {
+        OrderStatuses readyToShipStatus = new OrderStatuses();
+        readyToShipStatus.setStatusId(7); // 7 = ReadyToShip
+        order.setStatus(readyToShipStatus);
+        order.setEstimatedDeliveryDate(estimatedDeliveryDate); 
+        orderRepository.save(order);
+    }
+    return order;
+}
+
+
+    public void notifyDelivery(Integer orderId) {
+        // Logic gửi thông báo
+    }
+}
