d8b40be16220ba88a303139bf4df6a8598cc3819
anhNQ279 <anhnqhe181014@fpt.edu.vn>
2025-06-14 23:26:25 +0700

    Create ProductDetails.java

---
+++ BACKEND/src/main/java/com/spring/entity/ProductDetails.java
@@ -0,0 +1,29 @@
+package com.spring.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ProductDetails")
+public class ProductDetails {
+    @Id
+    @Column(name = "ProductID")
+    private Integer productId;
+
+    @Column(name = "HarvestDate")
+    private java.sql.Date harvestDate;
+
+    @Column(name = "ExpirationDate")
+    private java.sql.Date expirationDate;
+
+    @Column(name = "NutritionalInfo")
+    private String nutritionalInfo;
+
+    @Column(name = "Status")
+    private Integer status;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "ProductID")
+    private Products product;
+
+    }
